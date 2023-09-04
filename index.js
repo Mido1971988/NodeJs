debd0646c1f9abb737f4caaf3b3bed30f30384c6
Mido1971988 <mohammed.soliman1971988ca@gmail.com>
2023-09-04 19:57:55 +0300

    daily

--- index.js
+++ index.js
@@ -244,26 +244,26 @@ const path = require("node:path");
 // });
 
 // -------- Thread loop
-const crypto = require("crypto");
-const https = require("https");
+// const crypto = require("crypto");
+// const https = require("https");
 
-process.env.UV_THREADPOOL_SIZE = 4;
+// process.env.UV_THREADPOOL_SIZE = 4;
 
-const start = Date.now();
+// const start = Date.now();
 
-const MAX_CALLS = 4;
+// const MAX_CALLS = 12;
 
-for (let i = 0; i < MAX_CALLS; i++) {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-        console.log(`Hash: ${i + 1}`, Date.now() - start);
-    });
+// for (let i = 0; i < MAX_CALLS; i++) {
+//     // crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+//     //     console.log(`Hash: ${i + 1}`, Date.now() - start);
+//     // });
 
-    // https
-    //     .request("https://www.google.com", (res) => {
-    //     res.on("data", () => {});
-    //     res.on("end", () => {
-    //         console.log(`Request: ${i + 1}`, Date.now() - start);
-    //     });
-    //     })
-    //     .end();
-}
+//     https
+//         .request("https://www.google.com", (res) => {
+//         res.on("data", () => {});
+//         res.on("end", () => {
+//             console.log(`Request: ${i + 1}`, Date.now() - start);
+//         });
+//         })
+//         .end();
+// }
